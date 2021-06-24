a1ec21d61c91640db9b691b3ce1efddaa41794fd
Claudio Cifuentes <cacifuentesalonso@gmail.com>
2021-06-24 18:45:03 -0500

    Application code

---
+++ src/main/java/com/claudio/oauth2/photoswebclient/PhotosWebClientApplication.java
@@ -0,0 +1,13 @@
+package com.claudio.oauth2.photoswebclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotosWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotosWebClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/claudio/oauth2/photoswebclient/controller/AlbumsController.java
@@ -0,0 +1,51 @@
+package com.claudio.oauth2.photoswebclient.controller;
+
+import com.claudio.oauth2.photoswebclient.model.Album;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class AlbumsController {
+
+    @Autowired
+    private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    @Autowired
+    private WebClient webClient;
+
+    @GetMapping("/albums")
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal){
+
+        String url = "http://localhost:8082/albums";
+
+        List<Album> albums = webClient.get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Album>>(){})
+                .block();
+
+        model.addAttribute("albums", albums);
+
+        return "albums";
+    }
+}
